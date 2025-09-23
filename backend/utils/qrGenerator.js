64234f68ec5d4f4bc9b2f37873dd5f190777c5ab
ayushi-shahi <ayushishaho14072004@gmail.com>
2025-09-23 22:38:18 +0530

    QR changes

--- backend/utils/qrGenerator.js
+++ backend/utils/qrGenerator.js
@@ -1,7 +1,6 @@
 const QRCode = require('qrcode');
 
 async function generateDataUrl(payload) {
-  // payload should be small-ish; in production encrypt and/or host on a public endpoint
   const str = JSON.stringify(payload);
   return await QRCode.toDataURL(str, { errorCorrectionLevel: 'H' });
 }
